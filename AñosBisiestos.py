aa489f369452fca7c7870c2bca27e221ca6a7339
stivenrodriguezm <stivenrodriguez1996m@gmail.com>
2023-08-21 19:41:36 -0500

    actualización al codigo de años bisiestos. Sigue fincionando bien.

--- AñosBisiestos.py
+++ AñosBisiestos.py
@@ -2,12 +2,12 @@
 print("Introduce un año")
 year=int(input())
 
-if year:
-    if year%100 == 0 and year%400 == 0:
-        print(str(year) +" es bisiesto")
-    elif year%100 == 0 and year%400 != 0:
-        print(str(year) +" no es bisiesto")
-    elif year%4 == 0:
-        print(str(year) +" es bisiesto")
-    else:
-        print(str(year) + " no es bisiesto")
+
+if year%100 == 0 and year%400 == 0:
+    print(str(year) +" es bisiesto")
+elif year%100 == 0 and year%400 != 0:
+    print(str(year) +" no es bisiesto")
+elif year%4 == 0:
+    print(str(year) +" es bisiesto")
+else:
+    print(str(year) + " no es bisiesto")
